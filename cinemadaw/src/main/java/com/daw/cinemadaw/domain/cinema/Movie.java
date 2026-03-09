1bf1877ddf778aa5c756681a63c8ef99476d4733
WalidPr99 <WAAJ689@vidalibarraquer.net>
2026-03-09 09:55:11 +0100

    09/03/2026

--- cinemadaw/src/main/java/com/daw/cinemadaw/domain/cinema/Movie.java
+++ cinemadaw/src/main/java/com/daw/cinemadaw/domain/cinema/Movie.java
@@ -1,11 +1,14 @@
 package com.daw.cinemadaw.domain.cinema;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDate;
 
 @Entity
 public class Movie {
@@ -27,7 +30,8 @@ public class Movie {
     @Column
     private String description;
 
-    @Column
+    @Column(name = "release_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
     
     public Movie() {
